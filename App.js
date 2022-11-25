39471de782db5df367290c9d504dc89a5c788b4e
Jaqueline0806 <jaquelinevalencia022@gmail.com>
2022-11-24 19:35:35 -0600

    se agrega las practicas

---
+++ App.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import HomeScreen from './src/component/navigation/ScreenStack/HomeScreen';
+import SettingsScreen from './src/component/navigation/ScreenStack/SettingsScreen';
+import Extra from './src/component/navigation/ScreenStack/Extra';
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Ajustes') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Ajustes" component={SettingsScreen} />
+        <Tab.Screen name="Para ti" component={Extra} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+  
+}
